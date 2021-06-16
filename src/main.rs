b0b3eb33e2542637849d5d8e3db766179811e03b
Livi <livmaertens@gmail.com>
2021-06-16 16:54:02 +0200

    basic filtering mechanism

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::collections::HashMap;
 use std::fs;
-use std::error::Error;
+use rocket::State;
+use rocket::Responder;
+use rocket_contrib::json::Json;
+use serde_json::ser;
+use serde_json::json;
+
+//TO-DO:
+// What does responder do?
+
 
 
 #[derive(Serialize, Deserialize)]
@@ -25,9 +33,13 @@ struct Poll {
     date: String,
     description: String,
     sources: Vec<String>,
-    votes: serde_json::Value,
+    votes: HashMap<String,Vec<u32>>,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Data {
+    data: HashMap<u32, Parliament>,
+}
 
 fn parse_polls() -> Result<HashMap<u32, Parliament>>{
     let contents = fs::read_to_string("src/data.json").expect("Unable to read file");
@@ -39,22 +51,26 @@ fn parse_polls() -> Result<HashMap<u32, Parliament>>{
 
 
 
-#[get("/list?<period>&<count>")]
-fn index(period: u32,count: u32) -> String {
-    let x: HashMap<u32,Parliament> = match parse_polls() {
-        Ok(v) => v,
-        Err(e) => panic!("{}", e),
-    };
+#[get("/list?<parliament_id>&<count>")]
+fn index(parliament_id: u32,count: u32, state: &State<Data>) -> String {
+    let mut poll: Vec<&Poll> = Vec::new();
+    let poll1 = &state.data[&parliament_id].periods[&97].polls[0];
+    let poll2 = &state.data[&parliament_id].periods[&97].polls[1];
+    poll.push(poll1);
+    poll.push(poll2);
 
-    format!("The ID is {}",x.len())
-
+    serde_json::to_string(&poll).unwrap()
 }
 
 
 
 #[launch]
 fn rocket() -> _ {
-
+    let x: HashMap<u32,Parliament> = match parse_polls() {
+        Ok(v) => v,
+        Err(e) => panic!("{}", e),
+    };
     rocket::build()
     .mount("/", routes![index])
+    .manage(Data{data: x })
 }
